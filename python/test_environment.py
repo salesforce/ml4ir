dde536cd17134255a41c41c9202b96a93d4ff94b
Callie Bradley <cbradley@salesforce.com>
2020-02-05 11:28:28 -0800

    Another local test, this time in venv

--- python/test_environment.py
+++ python/test_environment.py
@@ -14,7 +14,7 @@ def main():
 
     if system_major != required_major:
         raise TypeError(
-            "This project requires Python version {}. Found instead: Python version {}".format(required_major, sys.version)
+            "This project requires Python version {}. Found: Python version {}".format(required_major, sys.version)
         )
     else:
         print(">>> Development environment passes all tests!")
