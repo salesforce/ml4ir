e6f421c0dd8b696c51b43312346d1093778ce72c
Jake <jmannix@salesforce.com>
2020-03-17 09:15:13 -0700

    failing unit test updated: still fails, but now truly encodes what we *want* the proto builder to be doing.

--- jvm/ml4ir-inference/src/test/java/ml4ir/inference/tensorflow/SequenceExampleJavaBuilderTest.java
+++ jvm/ml4ir-inference/src/test/java/ml4ir/inference/tensorflow/SequenceExampleJavaBuilderTest.java
@@ -12,23 +12,42 @@ import org.tensorflow.example.SequenceExample;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class SequenceExampleJavaBuilderTest {
 
     @Test
     public void testSimpleSequenceExample() {
         String query = "a query string";
+
+        // TODO: this should encode that f1, f2, and f3 are in the model and have default values, but fake_feat is not
+        FeatureConfig featureConfig = FeatureConfig.apply();
+
         SequenceExampleJavaBuilder helper = new SequenceExampleJavaBuilder(
-                FeatureConfig.apply(), "", null, null, ImmutableMap.of("query_text", query));
+                featureConfig,
+                "ignored",
+                ImmutableMap.of(),
+                ImmutableMap.of(),
+                ImmutableMap.of("query_text", query));
         SequenceExample sequenceExample = helper
                 .addDoc("doc1",
-                        ImmutableMap.of("f1", 1f, "f2", 0.5f),
+                        ImmutableMap.of("f1", 1f, "f2", 0.1f, "f3", 0.01f),
                         ImmutableMap.of(),
                         ImmutableMap.of())
                 .addDoc("doc2",
-                        ImmutableMap.of("f1", 0.9f, "f2", 0.2f),
+                        ImmutableMap.of("f1", 2f, "f2", 0.2f, "f3", 0.02f),
+                        ImmutableMap.of(),
+                        ImmutableMap.of())
+                .addDoc("doc3",
+                        ImmutableMap.of(/* no f1 -> anything */"f2", 0.3f, "f3", 0.03f),
+                        ImmutableMap.of(),
+                        ImmutableMap.of())
+                .addDoc("doc4",
+                        ImmutableMap.of("f1", 4f, "fake_feat", -1f /* no f2 or f3 */),
+                        ImmutableMap.of(),
+                        ImmutableMap.of())
+                .addDoc("doc5",
+                        ImmutableMap.of("f1", 5f, "f2", 0.5f, "f3", 0.05f),
                         ImmutableMap.of(),
                         ImmutableMap.of())
                 .build();
@@ -39,10 +58,15 @@ public class SequenceExampleJavaBuilderTest {
         List<Float> floatFeature1 =
                 featureListMap.get("f1").getFeatureList().get(0).getFloatList().getValueList();
         assertArrayEquals("feature f1 should match",
-                floatFeature1.toArray(new Float[0]), new Float[] { 1f, 0.9f });
+                floatFeature1.toArray(new Float[0]), new Float[] { 1f, 2f, 0f, 4f, 5f });
         List<Float> floatFeature2 =
                 featureListMap.get("f2").getFeatureList().get(0).getFloatList().getValueList();
         assertArrayEquals("feature f2 should match",
-                floatFeature2.toArray(new Float[0]), new Float[] { 0.5f, 0.2f });
+                floatFeature2.toArray(new Float[0]), new Float[] { 0.1f, 0.2f, 0.3f, 0f, 0.5f });
+        List<Float> floatFeature3 =
+                featureListMap.get("f3").getFeatureList().get(0).getFloatList().getValueList();
+        assertArrayEquals("feature f3 should match",
+                floatFeature3.toArray(new Float[0]), new Float[] { 0.01f, 0.02f, 0.03f, 0f, 0.5f });
+        assertNull(featureListMap.get("fake_feat"));
     }
 }
