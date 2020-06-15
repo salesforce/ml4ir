07ae50358cb9e30f03042e27f5251b6c04991264
dshah <dshah@salesforce.com>
2020-06-15 13:17:32 -0700

    Updating the setup file for packaging

--- python/setup.py
+++ python/setup.py
@@ -5,14 +5,29 @@ def load_dependencies():
         required = f.read().splitlines()
     return required
 
+
+def getReadMe():
+    with open("README.md", "r") as f:
+        long_description = f.read()
+    return long_description
+
 setup(
     name="ml4ir",
     packages=find_namespace_packages(include=["ml4ir.*"]),
     version="0.0.1",
     description="Machine Learning libraries for Information Retrieval",
+    long_description=getReadMe(),
+    long_description_content_type="text/markdown",
     author="Search Relevance, Salesforce",
-    author_email="searchrelevancyscrumteam@salesforce.com ",
-    data_files=[("ml4ir/build", ["ml4ir/build/Dockerfile"])],
+    author_email="searchrelevancyscrumteam@salesforce.com",
+    url="https://www.salesforce.com/",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+    include_package_data=True,
+    data_files=[("ml4ir/build", ["ml4ir/build/Dockerfile", "ml4ir/build/run_driver.sh"])],
     license="ASL 2.0",
+    python_requires='>=3.6',
     install_requires=load_dependencies()
 )
