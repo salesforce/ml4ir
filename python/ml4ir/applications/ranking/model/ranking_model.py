3ada5cb81aa42c928936dd11f783bb357fe36939
lastmansleeping <ashishbs7@gmail.com>
2020-06-29 10:55:10 -0700

    Updating pandas logging threshold

--- python/ml4ir/applications/ranking/model/ranking_model.py
+++ python/ml4ir/applications/ranking/model/ranking_model.py
@@ -12,6 +12,9 @@ from ml4ir.applications.ranking.model.metrics import metrics_helper
 
 from typing import Optional
 
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
+
 
 class RankingConstants:
     NEW_RANK = "new_rank"
