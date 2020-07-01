ae15a9b8ecb9661763445436e02ab4b44e5f4586
lastmansleeping <ashishbs7@gmail.com>
2020-07-01 02:57:02 -0700

    Updating tests

--- python/ml4ir/applications/ranking/tests/test_create_dataset.py
+++ python/ml4ir/applications/ranking/tests/test_create_dataset.py
@@ -6,21 +6,22 @@ import warnings
 from ml4ir.base.io import file_io
 from ml4ir.base.io.logging_utils import setup_logging
 from ml4ir.applications.ranking.data.scripts.create_dataset import run_dataset_creation
+
 warnings.filterwarnings("ignore")
 
 ROOT_DATA_DIR = "ml4ir/applications/ranking/tests/data/csv/train"
-FEATURE_CONFIG = "ml4ir/applications/ranking/tests/data/csv/synthetic/feature_config.yaml"
+FEATURE_CONFIG = "ml4ir/applications/ranking/tests/data/config/feature_config.yaml"
 OUTPUT_DIR = "ml4ir/applications/ranking/tests/test_output/synthetic"
-LOG_DIR = 'ml4ir/applications/ranking/tests/test_output_log/'
+LOG_DIR = "ml4ir/applications/ranking/tests/test_output_log/"
 
 
 class RankingCreateDatasetTest(unittest.TestCase):
     def setUp(
-            self,
-            root_data_dir: str = ROOT_DATA_DIR,
-            feature_config: str = FEATURE_CONFIG,
-            output_dir: str = OUTPUT_DIR,
-            log_dir: str = LOG_DIR
+        self,
+        root_data_dir: str = ROOT_DATA_DIR,
+        feature_config: str = FEATURE_CONFIG,
+        output_dir: str = OUTPUT_DIR,
+        log_dir: str = LOG_DIR,
     ):
         self.root_data_dir = root_data_dir
         self.feature_config = feature_config
@@ -34,30 +35,34 @@ class RankingCreateDatasetTest(unittest.TestCase):
 
     def test_synthetic_data(self):
 
-        feature_highval = {'text_match_bool':[0,1]}
+        feature_highval = {"text_match_bool": [0, 1]}
         max_num_records = 20
         num_samples = 10
 
-        df = run_dataset_creation(self.root_data_dir,
-                                  self.output_dir,
-                                  self.feature_config,
-                                  feature_highval,
-                                  max_num_records,
-                                  num_samples,
-                                  random_state=123)
-        assert len(df) == 37
-        assert df.query_str.nunique() == num_samples
+        df = run_dataset_creation(
+            self.root_data_dir,
+            self.output_dir,
+            self.feature_config,
+            feature_highval,
+            max_num_records,
+            num_samples,
+            random_state=123,
+        )
+        assert len(df) == 32
+        assert df.query_id.nunique() == num_samples
         assert df.num_results_calc.max() <= max_num_records
-        assert 'text_match_bool' in list(df.columns)
-        assert list(df.text_match_bool.unique()) == [0,1]
+        assert "text_match_bool" in list(df.columns)
+        assert list(df.text_match_bool.unique()) == [0, 1]
 
-        df_2 = run_dataset_creation(self.root_data_dir,
-                                    self.output_dir,
-                                    self.feature_config,
-                                    feature_highval,
-                                    max_num_records=2,
-                                    num_samples=10,
-                                    random_state = 123)
+        df_2 = run_dataset_creation(
+            self.root_data_dir,
+            self.output_dir,
+            self.feature_config,
+            feature_highval,
+            max_num_records=2,
+            num_samples=10,
+            random_state=123,
+        )
         assert len(df_2) == 20
 
     def tearDown(self):
