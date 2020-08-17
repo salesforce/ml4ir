e97edb744d2909a906bb5ceac513beea5a42dbb9
Mohamed A. Zahran <moh.a.zahran@gmail.com>
2020-08-17 15:42:15 -0700

    Updating the script to account for comments.

--- python/ml4ir/base/data/ranklib_to_ml4ir.py
+++ python/ml4ir/base/data/ranklib_to_ml4ir.py
@@ -1,42 +1,60 @@
 import pandas as pd
 import argparse
+import logging
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--inputfilePath', type=str, default='', help='ranklib input file path')
-parser.add_argument('--outputfilePath', type=str, default='', help='output converted file path')
-parser.add_argument('--keepAdditionalInfo', type=int, default=1, help='Option to keep additional info (All info after the "#") 1 to keep, 0 to ignore')
-parser.add_argument('--GL2Clicks', type=int, default=1, help='Convert graded relevance to clicks (only max relevant document is considered clicked) 1 to convert')
-args = parser.parse_args()
-query_id_name = 'qid'
-relevace_name = 'relevance'
+#Constanst for the column names given to the query id and graded relevance.
+QUERY_ID_NAME = 'qid'
+RELEVANCE_NAME = 'relevance'
 
-f = open(args.inputfilePath, 'r')
-cntr = 0
-rows = []
-for line in f:
-    if args.keepAdditionalInfo == 1:
-        feature_values = line.replace('#','').replace(' = ', ':').strip().split()
+
+def process_line(line, keep_additional_info):
+    if keep_additional_info == 1:
+        feature_values = line.replace('#', '').replace(' = ', ':').strip().split()
     else:
         feature_values = line.split('#')[0].strip().split()
-    feature_values[0] = relevace_name+':'+feature_values[0]
-    r={}
+    feature_values[0] = RELEVANCE_NAME + ':' + feature_values[0]
+    r = {}
     for fv in feature_values:
         feat = fv.split(':')[0].strip()
         val = fv.split(':')[1].strip()
-        if feat == relevace_name or feat == query_id_name:
+        if feat == RELEVANCE_NAME or feat == QUERY_ID_NAME:
             r[feat] = val
         else:
-            r['f_'+feat] = val
-    rows.append(r)
-        #r = {'f_'+fv.split(':')[0].strip() : fv.split(':')[1].strip() for fv in feature_values}
+            r['f_' + feat] = val
+    return r
 
-f.close()
-df = pd.DataFrame(rows)
-if args.GL2Clicks == 1:
-    groups = df.groupby(query_id_name)
-    for gname, group in groups:
-        df.loc[df[query_id_name] == gname, relevace_name] = (df.loc[df[query_id_name] == gname].relevance == max(group.relevance)).astype(int)
-df.to_csv(args.outputfilePath)
-print('DONE!')
+def convert(input_file, output_file, keep_additional_info, gl_2_clicks):
+    f = open(input_file, 'r')
+    rows = []
+
+    for line in f:
+        rows.append(process_line(line, keep_additional_info))
+    f.close()
+
+    df = pd.DataFrame(rows)
+    if gl_2_clicks == 1:
+        groups = df.groupby(QUERY_ID_NAME)
+        for gname, group in groups:
+            df.loc[df[QUERY_ID_NAME] == gname, RELEVANCE_NAME] = (
+                        df.loc[df[QUERY_ID_NAME] == gname].relevance == max(group.relevance)).astype(int)
+    df.to_csv(output_file)
 
 
+if __name__ == "__main__":
+    # Setup logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logging.debug("Logger is initialized...")
+
+    # parsing arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', type=str, default='ml4ir/applications/ranking/tests/data/csv/ranklib_sample.txt', help='ranklib input file path')
+    parser.add_argument('--output_file', type=str, default='ml4ir/applications/ranking/tests/data/csv/ranklib_sample_ml4ir.csv', help='output converted file path')
+    parser.add_argument('--keep_additional_info', type=int, default=1,
+                        help='Option to keep additional info (All info after the "#") 1 to keep, 0 to ignore')
+    parser.add_argument('--gl_2_clicks', type=int, default=1,
+                        help='Convert graded relevance to clicks (only max relevant document is considered clicked) 1 to convert')
+    args = parser.parse_args()
+
+    convert(args.input_file, args.output_file, args.keep_additional_info, args.gl_2_clicks)
+    logging.info('Conversion is completed')
