60894dd2c2f07a7f79fa8bde8bb1c082d7b07c4e
lastmansleeping <ashishbs7@gmail.com>
2020-09-23 08:40:12 -0700

    Updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 from typing import List
 
 # Set python project root
-sys.path.insert(0, os.path.abspath("../python/"))
+sys.path.insert(0, os.path.abspath("../../python/"))
 
 # The master toctree document
 master_doc = "index"
