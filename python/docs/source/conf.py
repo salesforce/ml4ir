51847b0d07c0c31a4be34e9ff2c29f5b1b491c66
lastmansleeping <ashishbs7@gmail.com>
2020-09-23 06:44:39 -0700

    Updating docs

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -33,7 +33,7 @@ release = "0.1.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions: List = []
+extensions: List = ["recommonmark", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path: List = ["_templates"]
