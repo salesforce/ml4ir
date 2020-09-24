Fix incorrect input dimension computation with categorical_embedding_to_encoding_bilstm.

python/ml4ir/base/features/feature_fns/categorical.py
@@ -155,13 +155,13 @@ def categorical_embedding_to_encoding_bilstm(feature_tensor, feature_info, file_
 
     (
         categorical_indices,
-        vocabulary_keys,
+        vocabulary_size,
         num_oov_buckets,
     ) = categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_io)
 
-    vocabulary_size = len(set(vocabulary_keys))
+    input_dim = vocabulary_size + num_oov_buckets if num_oov_buckets else vocabulary_size
     categorical_embeddings = layers.Embedding(
-        input_dim=vocabulary_size + num_oov_buckets,
+        input_dim=input_dim,
         output_dim=args["embedding_size"],
         mask_zero=True,
         input_length=args.get("max_length"),
@@ -203,10 +203,9 @@ def categorical_embedding_with_vocabulary_file(feature_tensor, feature_info, fil
     """
     (
         categorical_indices,
-        vocabulary_keys,
+        vocabulary_size,
         num_oov_buckets,
     ) = categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_io)
-    vocabulary_size = len(set(vocabulary_keys))
     feature_info_new = copy.deepcopy(feature_info)
     feature_info_new["feature_layer_info"]["args"]["num_buckets"] = (
         vocabulary_size + num_oov_buckets
@@ -218,6 +217,70 @@ def categorical_embedding_with_vocabulary_file(feature_tensor, feature_info, fil
     )
 
     return embedding
+
+
+def word_sequence_embedding_with_vocabulary_file(feature_tensor, feature_info, file_io: FileIO):
+    # This is what we want:
+    # word_ids = Input(shape=(None, ), name='word_ids')
+    # word_embeddings = Embedding(word_limit + PADDING_SIZE + OOV_SIZE,
+    #                         WORD_EMBEDDING_SIZE,  trainable=False)(word_ids)
+    # word_embeddings = Bidirectional(LSTM(WORD_LSTM_SIZE,  kernel_initializer='ones'))(word_embeddings)
+    # word_embeddings = Dropout(DROPOUT)(word_embeddings)
+    # word_embeddings = Dense(WORD_LSTM_DENSE, activation='relu')(word_embeddings)
+    # word_embeddings = Dropout(DROPOUT)(word_embeddings)
+    from tensorflow import io
+    from tensorflow import image
+    args = feature_info.get("feature_layer_info")["args"]
+
+    max_length = 20
+    word_lstm_size = 128
+    print(feature_tensor)
+    # tokens = tf.strings.split(feature_tensor, sep=" ")
+    # tokens = tokens.to_tensor()
+    # indices = lookup_table(tokens)
+    # padded_tokens = image.pad_to_bounding_box(tf.expand_dims(indices[:, :max_length], axis=-1),
+    #                                           offset_height=0,
+    #                                           offset_width=0,
+    #                                           target_height=1,
+    #                                           target_width=max_length)
+    # padded_tokens = tf.squeeze(padded_tokens, axis=-1)
+
+    # feature_tensor2 = tf.squeeze(feature_tensor, axis=1)
+    # print(feature_tensor2)
+
+    vocabulary_size, num_oov_buckets, padded_tokens = categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_io)
+
+    word_embedding = layers.Embedding(
+            input_dim=max_length,
+            output_dim=args["embedding_size"],
+            mask_zero=True,
+            input_length=args.get("max_length", None),
+        )(padded_tokens)
+
+    word_embedding_squeezed = tf.squeeze(word_embedding, axis=1)
+
+    encoding = get_bilstm_encoding2(word_embedding_squeezed, word_lstm_size)
+
+    return encoding
+
+
+def get_bilstm_encoding2(embedding, units):
+    """
+    Builds a bilstm on to on the embedding passed as input.
+    """
+    encoding = layers.Bidirectional(
+        layers.LSTM(units,
+                    kernel_initializer='ones'
+                    # units=units, return_sequences=False,
+        )
+    )(embedding)
+    # merge_mode="concat",
+    encoding = tf.expand_dims(encoding, axis=1)
+    return encoding
+
+
+
+
 
 
 class CategoricalDropout(layers.Layer):
@@ -301,11 +364,9 @@ def categorical_embedding_with_vocabulary_file_and_dropout(
     feature_layer_info = feature_info.get("feature_layer_info")
     (
         feature_tensor_indices,
-        vocabulary_keys,
+        vocabulary_size,
         num_oov_buckets,
     ) = categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_io)
-
-    vocabulary_size = len(set(vocabulary_keys))
 
     # Mask indices to OOV key of 0 at dropout_rate
     feature_tensor_indices = CategoricalDropout(
@@ -392,11 +453,9 @@ def categorical_indicator_with_vocabulary_file(feature_tensor, feature_info, fil
     #
     (
         feature_tensor_indices,
-        vocabulary_keys,
+        vocabulary_size,
         num_oov_buckets,
     ) = categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_io)
-
-    vocabulary_size = len(set(vocabulary_keys))
 
     categorical_identity_fc = feature_column.categorical_column_with_identity(
         CATEGORICAL_VARIABLE, num_buckets=vocabulary_size + num_oov_buckets
@@ -431,10 +490,15 @@ def categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_
 
     if "dropout_rate" in args:
         default_value = 0  # Default OOV index when using dropout
+        if args.get("num_oov_buckets"):
+            raise RuntimeError("Cannot have both dropout_rate and num_oov_buckets set. "
+                               "OOV buckets are not supported with dropout")
         num_oov_buckets = None
+        vocabulary_size = len(set(vocabulary_keys)) + 1  # one more for default value
     else:
         default_value = None
         num_oov_buckets = args.get("num_oov_buckets", 1)
+        vocabulary_size = len(set(vocabulary_keys))
 
     lookup_table = VocabLookup(
         vocabulary_keys=vocabulary_keys,
@@ -444,7 +508,8 @@ def categorical_indices_from_vocabulary_file(feature_info, feature_tensor, file_
         feature_name=feature_info.get("node_name", feature_info["name"]),
     )
     categorical_indices = lookup_table(feature_tensor)
-    return categorical_indices, vocabulary_keys, num_oov_buckets
+
+    return categorical_indices, vocabulary_size, num_oov_buckets
 
 
 class VocabLookup(layers.Layer):