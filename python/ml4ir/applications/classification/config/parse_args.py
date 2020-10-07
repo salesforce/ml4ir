db8e41ca1fa8eb9db023409ae422eef4d4072211
Georgios Balikas <gbalikas@salesforce.com>
2020-10-07 12:42:13 +0200

    trying to add new metric (TopKCategoricalAccuracy) (#67)

--- python/ml4ir/applications/classification/config/parse_args.py
+++ python/ml4ir/applications/classification/config/parse_args.py
@@ -17,9 +17,8 @@ class ClassificationArgParser(RelevanceArgParser):
         self.set_defaults(
             tfrecord_type=TFRecordTypeKey.EXAMPLE,
             loss_key=LossKey.CATEGORICAL_CROSS_ENTROPY,
-            metrics_keys="['" + MetricKey.CATEGORICAL_ACCURACY + "'"
-                         + ", '" + MetricKey.PRECISION + "'"
-                         + "]",
+            metrics_keys="['{}', '{}']".format(MetricKey.CATEGORICAL_ACCURACY,
+                                               MetricKey.TOP_5_CATEGORICAL_ACCURACY),
             monitor_metric=MetricKey.CATEGORICAL_ACCURACY,
             monitor_mode="max",
             group_metrics_min_queries=25,
