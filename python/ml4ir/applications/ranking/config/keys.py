e0fb90feb73ddbdbc2c066c0e0b198c9a99738d9
lastmansleeping <ashishbs7@gmail.com>
2020-10-07 04:17:59 -0700

    Removing top 5 categorical accuracy from ranking metrics

--- python/ml4ir/applications/ranking/config/keys.py
+++ python/ml4ir/applications/ranking/config/keys.py
@@ -26,7 +26,6 @@ class MetricKey(Key):
     NDCG = "NDCG"
     PRECISION = "Precision"
     CATEGORICAL_ACCURACY = "categorical_accuracy"
-    TOP_5_CATEGORICAL_ACCURACY = "top_5_categorical_accuracy"
 
 
 class LossTypeKey(Key):
