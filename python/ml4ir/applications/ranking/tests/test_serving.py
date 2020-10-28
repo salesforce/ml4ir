fe632471062eadbb43a55fefb86ad3d160cc1416
Georgios Balikas <gbalikas@salesforce.com>
2020-10-28 15:07:19 +0100

    revert removing test; memory issue in circleci?

--- python/ml4ir/applications/ranking/tests/test_serving.py
+++ python/ml4ir/applications/ranking/tests/test_serving.py
@@ -174,16 +174,16 @@ class RankingModelTest(RankingTestBase):
             except Exception:
                 assert False
 
-    # def test_serving_required_fields_only(self):
-    #     """Test serving signature with protos with only required fields"""
-    #     feature_config: FeatureConfig = self.get_feature_config()
-    #     tfrecord_signature = self.get_tfrecord_signature(feature_config)
-    #
-    #     proto = tf.constant(
-    #         [feature_config.create_dummy_protobuf(required_only=True).SerializeToString()]
-    #     )
-    #
-    #     try:
-    #         tfrecord_signature(protos=proto)
-    #     except Exception:
-    #         assert False
+    def test_serving_required_fields_only(self):
+        """Test serving signature with protos with only required fields"""
+        feature_config: FeatureConfig = self.get_feature_config()
+        tfrecord_signature = self.get_tfrecord_signature(feature_config)
+
+        proto = tf.constant(
+            [feature_config.create_dummy_protobuf(required_only=True).SerializeToString()]
+        )
+
+        try:
+            tfrecord_signature(protos=proto)
+        except Exception:
+            assert False
