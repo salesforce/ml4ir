59446783547f3fb2750c129c0c35ed08a1112ace
lastmansleeping <ashishbs7@gmail.com>
2020-10-29 00:38:11 -0700

    Changing config to configs in tests

--- python/ml4ir/applications/ranking/tests/test_ranking_model.py
+++ python/ml4ir/applications/ranking/tests/test_ranking_model.py
@@ -68,7 +68,7 @@ class RankingModelTest(RankingTestBase):
 
         # Test model training on CSV data
         data_dir = os.path.join(self.root_data_dir, "csv")
-        feature_config_path = os.path.join(self.root_data_dir, "config", self.feature_config_fname)
+        feature_config_path = os.path.join(self.root_data_dir, "configs", self.feature_config_fname)
 
         csv_loss, csv_mrr = self.run_default_pipeline(
             data_dir=data_dir, data_format="csv", feature_config_path=feature_config_path
@@ -80,7 +80,7 @@ class RankingModelTest(RankingTestBase):
 
         # Test model training on TFRecord SequenceExample data
         data_dir = os.path.join(self.root_data_dir, "tfrecord")
-        feature_config_path = os.path.join(self.root_data_dir, "config", self.feature_config_fname)
+        feature_config_path = os.path.join(self.root_data_dir, "configs", self.feature_config_fname)
 
         tfrecord_loss, tfrecord_mrr = self.run_default_pipeline(
             data_dir=data_dir, data_format="tfrecord", feature_config_path=feature_config_path
