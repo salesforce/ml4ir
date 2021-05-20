ffc8009d0869009bc57027c34a7bed87812c3ca4
Ashish Bharadwaj Srinivasa <ashishbs7@gmail.com>
2021-05-20 16:38:06 -0700

    Adding tf native fns to the feature_layer

---
+++ python/ml4ir/base/features/feature_fns/tf_native.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+
+from ml4ir.base.io.file_io import FileIO
+
+
+def tf_native_op(feature_tensor: tf.Tensor, feature_info: dict, file_io: FileIO):
+    """
+    Run a series of tensorflow native operations on the input feature tensor.
+
+    Parameters
+    ----------
+    feature_tensor : Tensor
+        Input feature tensor
+    feature_info : dict
+        Dictionary representing the configuration parameters for the specific feature from the FeatureConfig
+    file_io : FileIO object
+        FileIO handler object for reading and writing
+
+    Returns
+    -------
+    Tensor object
+        Modified feature tensor after applying all the specified ops
+
+    Notes
+    -----
+    Args under feature_layer_info:
+        list of dict
+            List of function specifications with associated arguments
+            
+            Arguments under opts:
+                fn : str
+                    Tensorflow native function name. Should start with tf.
+                    Example: tf.math.log or tf.clip_by_value
+                kwargs : dict
+                    Keyword arguments to be passed to the tensorflow function
+
+    * The functions will be applied in the order they are specified.
+    """
+    feature_node_name = feature_info.get("node_name", feature_info.get("name"))
+    tf_ops = feature_info.get("feature_layer_info", {}).get("args")
+
+    if not tf_ops:
+        return feature_tensor
+
+    for tf_op in tf_ops:
+        try:
+            fn_, kwargs = eval(tf_op["fn"]), tf_op.get("kwargs", {})
+        except AttributeError as e:
+            raise KeyError("Invalid fn specified for tf_native_op : {}\n{}".format(tf_op["fn"], e))
+        
+        try:
+            feature_tensor = fn_(feature_tensor, **kwargs)
+        except Exception as e:
+            raise Exception("Error while applying {} to {} feature:\n{}".format(tf_op["fn"], feature_node_name, e))
+
+    # Adjusting the shape to the default feature fns for concatenating in the next step
+    feature_tensor = tf.expand_dims(feature_tensor, axis=-1, name="{}_expanded".format(feature_node_name))
+
+    return feature_tensor
