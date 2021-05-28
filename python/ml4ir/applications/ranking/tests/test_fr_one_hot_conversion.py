093646b69556d0ad424ec5974bb10e241ff42e43
Mohamed Abdelrahman Zahran Mohamed <mohamed.m@salesforce.com>
2021-05-27 17:37:18 -0700

    Fixing one hot fr conversion testing

--- python/ml4ir/applications/ranking/tests/test_fr_one_hot_conversion.py
+++ python/ml4ir/applications/ranking/tests/test_fr_one_hot_conversion.py
@@ -13,16 +13,14 @@ class TestFrOneHotConversion(unittest.TestCase):
         one_hot = convert_fr_to_one_hot(fr_value, max_ranks, mask_fr)
         expected = np.zeros(max_ranks)
         if not mask_fr:
-            expected[fr_value - 1] = 1
+            expected[fr_value[0] - 1] = 1
         assert (one_hot.numpy() == expected).all()
 
     def test_fr_one_hot_conversion(self):
-        self.fr_one_hot_conversion([1], 25, False)
-        self.fr_one_hot_conversion([5], 25, True)
-        self.fr_one_hot_conversion(1, 5, False)
-        self.fr_one_hot_conversion(5, 5, True)
-        self.fr_one_hot_conversion(1, 2, True)
-        self.fr_one_hot_conversion(2, 2, False)
+        self.fr_one_hot_conversion([1], 5, False)
+        self.fr_one_hot_conversion([5], 5, True)
+        self.fr_one_hot_conversion([1], 2, True)
+        self.fr_one_hot_conversion([2], 2, False)
 
 
 if __name__ == "__main__":
