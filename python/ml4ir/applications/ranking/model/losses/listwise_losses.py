e0c04ab6fb9c232608426c12efce80d3b9b8c483
Ashish Bharadwaj Srinivasa <ashishbs7@gmail.com>
2021-09-30 23:05:09 -0700

    Adding softmax cross entropy loss

--- python/ml4ir/applications/ranking/model/losses/listwise_losses.py
+++ python/ml4ir/applications/ranking/model/losses/listwise_losses.py
@@ -6,7 +6,75 @@ from tensorflow.keras.losses import Reduction
 from ml4ir.applications.ranking.model.losses.loss_base import ListwiseLossBase
 
 
-class RankOneListNet(ListwiseLossBase):
+class SoftmaxCrossEntropyLoss(ListwiseLossBase):
+    def get_loss_fn(self, **kwargs):
+        """
+        Define a masked rank 1 ListNet loss
+        Additionally can pass in record positions to handle positional bias
+
+        Returns
+        -------
+        function
+            Function to compute top 1 listnet loss
+
+        Notes
+        -----
+            Uses `mask` field to exclude padded records from contributing
+            to the loss
+        """
+        cce = losses.CategoricalCrossentropy()
+        mask = kwargs.get("mask")
+
+        def _loss_fn(y_true, y_pred):
+            """
+            Shapes
+            ------
+            y_true : [batch_size, num_classes, 1]
+            y_pred : [batch_size, num_classes, 1]
+            mask : [batch_size, num_classes, 1]
+            """
+            return cce(y_true, tf.math.multiply(y_pred, mask))
+
+        return _loss_fn
+
+    def get_final_activation_op(self, output_name):
+        """
+        Define a masked softmax activation function
+
+        Parameters
+        ----------
+        output_name : str
+            Name of the output to apply softmax activation on
+
+        Returns
+        -------
+        function
+            Function to compute masked softmax
+
+        Notes
+        -----
+            Uses `mask` field to exclude padded records from contributing
+            to the softmax activation
+        """
+        softmax_op = layers.Softmax(axis=-1, name=output_name)
+
+        def masked_softmax(logits, mask):
+            """
+            NOTE:
+            Tried to manually compute softmax with tf operations,
+            but tf.keras.layers.Softmax() is more stable when working with
+            cross_entropy layers
+            """
+            logits = tf.where(
+                tf.equal(mask, tf.constant(1.0)), logits, tf.constant(tf.float32.min)
+            )
+
+            return softmax_op(logits)
+
+        return masked_softmax
+
+
+class RankOneListNet(SoftmaxCrossEntropyLoss):
     def get_loss_fn(self, **kwargs):
         """
         Define a masked rank 1 ListNet loss
@@ -47,39 +115,3 @@ class RankOneListNet(ListwiseLossBase):
             return tf.math.divide(bce(y_true, y_pred), batch_size)
 
         return _loss_fn
-
-    def get_final_activation_op(self, output_name):
-        """
-        Define a masked softmax activation function
-
-        Parameters
-        ----------
-        output_name : str
-            Name of the output to apply softmax activation on
-
-        Returns
-        -------
-        function
-            Function to compute masked softmax
-
-        Notes
-        -----
-            Uses `mask` field to exclude padded records from contributing
-            to the softmax activation
-        """
-        softmax_op = layers.Softmax(axis=-1, name=output_name)
-
-        def masked_softmax(logits, mask):
-            """
-            NOTE:
-            Tried to manually compute softmax with tf operations,
-            but tf.keras.layers.Softmax() is more stable when working with
-            cross_entropy layers
-            """
-            logits = tf.where(
-                tf.equal(mask, tf.constant(1.0)), logits, tf.constant(tf.float32.min)
-            )
-
-            return softmax_op(logits)
-
-        return masked_softmax
