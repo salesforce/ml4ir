363ec3ef359714c0b54110419381d8bac2c3316b
Ashish Bharadwaj Srinivasa <ashishbs7@gmail.com>
2021-10-21 14:16:56 -0700

    Fixing issues

--- python/ml4ir/applications/ranking/model/ranking_model.py
+++ python/ml4ir/applications/ranking/model/ranking_model.py
@@ -364,7 +364,8 @@ class LinearRankingModel(RankingModel):
         postprocessing_fn=None,
         required_fields_only: bool = True,
         pad_sequence: bool = False,
-        dataset: Optional[RelevanceDataset] = None
+        dataset: Optional[RelevanceDataset] = None,
+        experiment_details: Optional[dict] = None
     ):
         """
         Save the RelevanceModel as a tensorflow SavedModel to the `models_dir`
@@ -392,6 +393,8 @@ class LinearRankingModel(RankingModel):
             Set this to False if you want to not handle padded scores.
         dataset : `RelevanceDataset` object
             RelevanceDataset object that can optionally be used for saving
+        experiment_details: dict
+            Dictionary containing metadata and results about the current experiment
 
         Notes
         -----
