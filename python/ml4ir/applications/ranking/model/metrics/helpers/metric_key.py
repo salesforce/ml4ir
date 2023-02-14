408a7dd3b31efcbc6ac2bd218afd307b875d7580
Mohamed Abdelrahman Zahran Mohamed <mohamed.m@salesforce.com>
2023-02-14 04:38:07 -0800

    Adding doc strings

--- python/ml4ir/applications/ranking/model/metrics/helpers/metric_key.py
+++ python/ml4ir/applications/ranking/model/metrics/helpers/metric_key.py
@@ -28,6 +28,7 @@ class Metric:
 
     @staticmethod
     def get_all_metrics():
+        """returns all the metrics"""
         return[
             Metric.MRR,
             Metric.ACR,
@@ -37,6 +38,7 @@ class Metric:
 
     @staticmethod
     def get_all_aux_metrics():
+        """returns only the metrics related to auxiliary loss"""
         return [
             Metric.AUX_ALL_FAILURE,
             Metric.AUX_INTRINSIC_FAILURE,
@@ -44,9 +46,22 @@ class Metric:
 
     @staticmethod
     def get_metrics_with_new_old_prefix(metric_list):
+        """
+        Adds the prefix '_old', '_new' to the given metric list. This is needed for power analysis
+
+        Parameters
+        ----------
+        metric_list: list
+            A list of metrics
+
+        Returns
+        ----------
+        new_old_list: list
+            The list of metrics with the prefixes '_new', '_old' added.
+        """
         new_old_list = []
         for m in metric_list:
-            new_old_list.append("old_"+m)
+            new_old_list.append("old_" + m)
             new_old_list.append("new_" + m)
         return new_old_list
 
