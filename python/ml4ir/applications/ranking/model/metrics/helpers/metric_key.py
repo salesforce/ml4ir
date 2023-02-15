167943bf0748b01735303018d12cb3b6a47e3de8
Mohamed A. Zahran <moh.a.zahran@gmail.com>
2023-02-15 10:50:20 -0800

    refactor power analysis for AuxRankMF (#213)
    * refactor power analysis for AuxRankMF
    
    * Adding test
    
    * remove Pingouin dependency for unlikely 3pp approval.

--- python/ml4ir/applications/ranking/model/metrics/helpers/metric_key.py
+++ python/ml4ir/applications/ranking/model/metrics/helpers/metric_key.py
@@ -25,3 +25,44 @@ class Metric:
             Metric.AUX_INTRINSIC_FAILURE,
             Metric.AUX_RANKMF
         ]
+
+    @staticmethod
+    def get_all_metrics():
+        """returns all the metrics"""
+        return[
+            Metric.MRR,
+            Metric.ACR,
+            Metric.AUX_ALL_FAILURE,
+            Metric.AUX_INTRINSIC_FAILURE,
+            Metric.AUX_RANKMF]
+
+    @staticmethod
+    def get_all_aux_metrics():
+        """returns only the metrics related to auxiliary loss"""
+        return [
+            Metric.AUX_ALL_FAILURE,
+            Metric.AUX_INTRINSIC_FAILURE,
+            Metric.AUX_RANKMF]
+
+    @staticmethod
+    def get_metrics_with_new_old_prefix(metric_list):
+        """
+        Adds the prefix '_old', '_new' to the given metric list. This is needed for power analysis
+
+        Parameters
+        ----------
+        metric_list: list
+            A list of metrics
+
+        Returns
+        ----------
+        new_old_list: list
+            The list of metrics with the prefixes '_new', '_old' added.
+        """
+        new_old_list = []
+        for m in metric_list:
+            new_old_list.append("old_" + m)
+            new_old_list.append("new_" + m)
+        return new_old_list
+
+
