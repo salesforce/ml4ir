b203d855ef6c1923a0ffd464603db1ebb91e46b1
Mohamed A. Zahran <moh.a.zahran@gmail.com>
2024-01-11 20:10:00 -0800

    Adding Robust Scaler Normalization layer (#253)
    * Adding Robust Scaler Normalization layer
    
    * refactor

---
+++ python/ml4ir/applications/ranking/tests/test_robust_scaler_transform.py
@@ -0,0 +1,47 @@
+import unittest
+
+import numpy as np
+
+from ml4ir.base.model.layers.robust_scaler import RobustScalerLayer
+
+
+class TestRobustScalerLayer(unittest.TestCase):
+
+    def test_reciprocal_rank_default(self):
+        input_feature = np.array([[0.0, 0.8, 0.9, 0.5, 0.6, 0.7]])
+
+        percentile_25 = np.percentile(input_feature, 25)
+        percentile_75 = np.percentile(input_feature, 75)
+
+        robust_scaler_op = RobustScalerLayer(p25=percentile_25, p75=percentile_75)
+
+        actual_reciprocal_ranks = robust_scaler_op(input_feature).numpy()
+        expected_reciprocal_ranks = (input_feature - percentile_25) / (percentile_75 - percentile_25)
+
+        self.assertTrue(np.isclose(actual_reciprocal_ranks, expected_reciprocal_ranks).all())
+
+    def test_reciprocal_rank_negative_scores(self):
+        input_feature = np.array([[0.0, 0.8, 0.9, -0.5, -0.4, -0.3]])
+
+        percentile_25 = np.percentile(input_feature, 25)
+        percentile_75 = np.percentile(input_feature, 75)
+
+        robust_scaler_op = RobustScalerLayer(p25=percentile_25, p75=percentile_75)
+
+        actual_reciprocal_ranks = robust_scaler_op(input_feature).numpy()
+        expected_reciprocal_ranks = (input_feature - percentile_25) / (percentile_75 - percentile_25)
+
+        self.assertTrue(np.isclose(actual_reciprocal_ranks, expected_reciprocal_ranks).all())
+
+    def test_reciprocal_rank_zeros(self):
+        input_feature = np.array([[0.0, 0.0, 0.0, 0.0]])
+
+        percentile_25 = np.percentile(input_feature, 25)
+        percentile_75 = np.percentile(input_feature, 75)
+
+        robust_scaler_op = RobustScalerLayer(p25=percentile_25, p75=percentile_75)
+
+        actual_reciprocal_ranks = robust_scaler_op(input_feature).numpy()
+        expected_reciprocal_ranks = input_feature
+
+        self.assertTrue(np.isclose(actual_reciprocal_ranks, expected_reciprocal_ranks).all())
