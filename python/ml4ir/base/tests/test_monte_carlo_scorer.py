44e4d04c0b84e6910e9b9dddedbad1297339254a
Mohamed Abdelrahman Zahran Mohamed <moh.a.zahran@gmail.com>
2024-06-13 10:44:23 -0700

    Adding tests

--- python/ml4ir/base/tests/test_monte_carlo_scorer.py
+++ python/ml4ir/base/tests/test_monte_carlo_scorer.py
@@ -12,7 +12,7 @@ class TestMonteCarloScorer(unittest.TestCase):
         model_config = {'architecture_key': 'linear',
                         'layers': [{'type': 'dense', 'name': 'linear_layer', 'units': 1, 'activation': None}],
                         'optimizer': {'key': 'adam'}, 'lr_schedule': {'key': 'constant', 'learning_rate': 0.01},
-                        'monte_carlo_inference_trials': {'num_test_trials': 10, "num_training_trials": 10}}
+                        'monte_carlo_inference_trials': {'num_test_trials': 10, "num_training_trials": 5}}
 
         self.scorer = MonteCarloScorer(
             model_config=model_config,
@@ -24,7 +24,7 @@ class TestMonteCarloScorer(unittest.TestCase):
 
         self.scorer.architecture_op = MagicMock()
 
-    def test_10_calls(self):
+    def test_10_calls_for_testing(self):
         inputs = {"f0": tf.constant([0.5], dtype=tf.float32)}
 
         # Call the method
@@ -32,10 +32,10 @@ class TestMonteCarloScorer(unittest.TestCase):
 
         # Check that the call method has looped the correct number of times.
         # It checks the number of "addition" operation used in aggregating the MC trials.
-        self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_inference_trials)
+        self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_test_trials)
 
-    def test_0_calls(self):
-        self.scorer.monte_carlo_inference_trials = 0
+    def test_0_calls_for_testing(self):
+        self.scorer.monte_carlo_test_trials = 0
 
         inputs = {"f0": tf.constant([0.5], dtype=tf.float32)}
 
@@ -44,7 +44,29 @@ class TestMonteCarloScorer(unittest.TestCase):
 
         # Check that the call method has looped the correct number of times.
         # It checks the number of "addition" operation used in aggregating the MC trials.
-        self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_inference_trials)
+        self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_test_trials)
+
+    def test_10_calls_for_training(self):
+        inputs = {"f0": tf.constant([0.5], dtype=tf.float32)}
+
+        # Call the method
+        result = self.scorer.call(self.scorer, inputs)
+
+        # Check that the call method has looped the correct number of times.
+        # It checks the number of "addition" operation used in aggregating the MC trials.
+        self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_training_trials)
+
+    def test_0_calls_for_training(self):
+        self.scorer.monte_carlo_training_trials = 0
+
+        inputs = {"f0": tf.constant([0.5], dtype=tf.float32)}
+
+        # Call the method
+        result = self.scorer.call(self.scorer, inputs)
+
+        # Check that the call method has looped the correct number of times.
+        # It checks the number of "addition" operation used in aggregating the MC trials.
+        self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_training_trials)
 
     # TODO add test to check the actual scores
 
