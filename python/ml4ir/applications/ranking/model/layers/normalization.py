97beb6a6129cb09478fcefd9e20e662d5afa9d29
Ashish Bharadwaj Srinivasa <ashishbs7@gmail.com>
2024-06-17 00:05:05 -0700

    Adding reciprocal layer

--- python/ml4ir/applications/ranking/model/layers/normalization.py
+++ python/ml4ir/applications/ranking/model/layers/normalization.py
@@ -198,3 +198,69 @@ class TheoreticalMinMaxNormalization(layers.Layer):
             tf.math.subtract(query_max, self.theoretical_min))
 
         return normed_inputs
+
+
+class Reciprocal(layers.Layer):
+    """
+    Converts a tensor of scores into reciprocal scores.
+    Can optionally add a constant or variable k to decay
+
+    Final formulation of reciprocal  = 1 / (k + score)
+    """
+
+    def __init__(self,
+                 name="reciprocal",
+                 k: float = 0.0,
+                 k_trainable: bool = False,
+                 scale_to_one: bool = False,
+                 **kwargs):
+        """
+        Parameters
+        ----------
+        name: str
+            Layer name
+        k : float
+            Constant value to be added to the rank before reciprocal
+        k_trainable: bool
+            If k should be a learnable variable; will be initialized with value of k
+        scale_to_one: bool
+            If true, the values are scaled to (0, 1]
+            by multiplying the reciprocal ranks by k + 1
+        kwargs:
+            Additional key-value args that will be used for configuring the layer
+        """
+        self.k_trainable = k_trainable
+        self.k = tf.Variable(
+            initial_value=float(k),
+            trainable=self.k_trainable
+        )
+        self.scale_to_one = scale_to_one
+        super().__init__(name=name, **kwargs)
+
+    def call(self, inputs, training=None):
+        """
+        Defines the forward pass for the layer on the inputs tensor
+
+        Parameters
+        ----------
+        inputs: tensor
+            Input tensor on which the feature transforms are applied
+        training: boolean
+            Boolean flag indicating if the layer is being used in training mode or not
+
+        Returns
+        -------
+        tf.Tensor
+            Resulting tensor after the forward pass through the feature transform layer
+        """
+        # Add k to the scores
+        scores = tf.add(inputs, self.k)
+
+        # Reciprocal of the scores
+        reciprocals = tf.math.divide_no_nan(1.0, scores)
+
+        # Scale the reciprocal ranks from (0, 1]
+        if self.scale_to_one:
+            reciprocals = tf.math.multiply_no_nan(reciprocals, (self.k + 1.))
+
+        return reciprocals
