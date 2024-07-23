4bab28ce7181fb4145603b58dbfe7ad076949c9d
Mohamed Abdelrahman Zahran Mohamed <moh.a.zahran@gmail.com>
2024-07-23 12:37:27 -0700

    Adding unit test for fixed masks

--- python/ml4ir/base/tests/test_monte_carlo_scorer.py
+++ python/ml4ir/base/tests/test_monte_carlo_scorer.py
@@ -1,7 +1,9 @@
+import math
 import unittest
 from unittest.mock import MagicMock
 import tensorflow as tf
 from ml4ir.base.model.scoring.monte_carlo_scorer import MonteCarloScorer
+from ml4ir.applications.ranking.model.layers.masking import QueryFeatureMask
 
 
 class TestMonteCarloScorer(unittest.TestCase):
@@ -67,6 +69,7 @@ class TestMonteCarloScorer(unittest.TestCase):
         self.assertEqual(str(result["score"].name).count("iadd"), self.scorer.monte_carlo_training_trials)
 
     def test_fixed_mask_inputs(self):
+        self.model_config["monte_carlo_trials"] = {'use_fixed_mask_in_training': True, "use_fixed_mask_in_testing": True}
         self.scorer = MonteCarloScorer(
             model_config=self.model_config,
             feature_config=MagicMock(),
@@ -76,65 +79,23 @@ class TestMonteCarloScorer(unittest.TestCase):
         )
 
         self.scorer.architecture_op = MagicMock()
-        self.scorer.features_with_fixed_masks = ['feature1', 'feature2']
-        self.scorer.fixed_mask = [
-            [[1, 0], [0, 1]],
-            [[0, 1], [1, 0]]
-        ]
-        self.scorer.fixed_feature_count = 2
-
+        mask = QueryFeatureMask(name="query_feature_mask",
+                                 mask_rate=0,
+                                 mask_at_inference=True,
+                                 use_fixed_masks=True,
+                                 requires_mask=True)
+        batch_size = 4
+        sequence_len = 5
+        feature_dim = 3
+        f1 = tf.random.uniform((batch_size, sequence_len, feature_dim))
+        f2 = tf.random.uniform((batch_size, sequence_len, feature_dim))
+        mask(f1)
         inputs = {
-            'feature1': tf.constant([[1.0, 2.0], [3.0, 4.0]]),
-            'feature2': tf.constant([[5.0, 6.0], [7.0, 8.0]])
+            'feature1': tf.constant(f1),
+            'feature2': tf.constant(f2)
         }
-        mask_count = 2
-
-        expected_masked_inputs_list = [
-            {
-                'feature1': tf.constant([[1.0, 0.0], [3.0, 0.0]]),
-                'feature2': tf.constant([[0.0, 6.0], [0.0, 8.0]])
-            },
-            {
-                'feature1': tf.constant([[0.0, 2.0], [0.0, 4.0]]),
-                'feature2': tf.constant([[5.0, 0.0], [7.0, 0.0]])
-            }
-        ]
-        result = self.scorer.mask_inputs(inputs, mask_count)
-
-        for i in range(mask_count):
-            for key in inputs.keys():
-                self.assertTrue(tf.reduce_all(tf.equal(result[i][key], expected_masked_inputs_list[i][key])))
-
-    def test_reshape_and_normalize(self):
-        self.scorer = MonteCarloScorer(
-            model_config=self.model_config,
-            feature_config=MagicMock(),
-            interaction_model=MagicMock(),
-            loss=MagicMock(),
-            file_io=MagicMock()
-        )
-
-        self.scorer.architecture_op = MagicMock()
-        self.scorer.monte_carlo_fixed_trials_count = 2
-        mask_count = 2
-        batch_size = 3
-
-        # Create a mock all_scores tensor with shape [mask_count * batch_size, feature_count]
-        all_scores = tf.constant([
-            [1.0, 2.0], [3.0, 4.0], [5.0, 6.0],  # First mask
-            [7.0, 8.0], [9.0, 10.0], [11.0, 12.0]  # Second mask
-        ])  # Shape: [6, 2]
-
-        # Expected reshaped and normalized scores
-        expected_all_scores = tf.constant([
-            [4.0, 5.0],  # (1+7)/2, (2+8)/2
-            [6.0, 7.0],  # (3+9)/2, (4+10)/2
-            [8.0, 9.0]  # (5+11)/2, (6+12)/2
-        ])  # Shape: [3, 2]
-
-        result = self.scorer.reshape_and_normalize(all_scores, mask_count, batch_size)
-
-        self.assertTrue(tf.reduce_all(tf.equal(result, expected_all_scores)))
+        result = self.scorer.call(inputs, training=True)
+        self.assertEqual(str(result["score"].name).count("iadd"), int(math.pow(2,feature_dim)-1))
 
 
 if __name__ == "__main__":
