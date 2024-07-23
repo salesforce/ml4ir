Adding fixed masking to QueryFeatureMask

python/ml4ir/applications/ranking/model/layers/masking.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers
+import itertools
 
 
 class RecordFeatureMask(layers.Layer):
@@ -133,10 +134,13 @@ class QueryFeatureMask(layers.Layer):
                 [0., 1., 1., 1.]]])
     """
 
+    fixed_mask_count = 1
+
     def __init__(self,
                  name="query_feature_mask",
                  mask_rate: float = 0.2,
                  mask_at_inference: bool = False,
+                 use_fixed_masks: bool = False,
                  requires_mask: bool = False,
                  **kwargs):
         """
@@ -159,6 +163,9 @@ class QueryFeatureMask(layers.Layer):
         self.mask_rate = mask_rate
         self.mask_at_inference = mask_at_inference
         self.requires_mask = requires_mask
+        self.use_fixed_masks = use_fixed_masks
+        self.fixed_masks = None
+        self.current_mask_index = -1
 
     def call(self, inputs, mask=None, training=None):
         """
@@ -182,12 +189,24 @@ class QueryFeatureMask(layers.Layer):
             Shape: [batch_size, sequence_len, feature_dim]
         """
         batch_size, sequence_len, feature_dim = tf.shape(inputs)[0], tf.shape(inputs)[1], tf.shape(inputs)[2]
-        if self.mask_at_inference or training:
-            mask = tf.cast(tf.math.greater(tf.random.uniform([batch_size, feature_dim]), self.mask_rate), dtype=tf.float32)
-            mask = tf.expand_dims(mask, axis=1)
-            mask = tf.tile(mask, [1, sequence_len, 1])
-            return tf.multiply(inputs, mask)
+        if self.use_fixed_masks:
+            if self.fixed_masks is None:
+                self.fixed_masks = list(itertools.product([0, 1], repeat=feature_dim))
+                QueryFeatureMask.fixed_mask_count = len(self.fixed_masks)
+
+            current_mask = tf.expand_dims(tf.expand_dims(self.fixed_masks[self.current_mask_index], 0), 0)
+            current_mask = tf.tile(current_mask, [batch_size, sequence_len, 1])
+            self.current_mask_index += 1
+            self.current_mask_index %= QueryFeatureMask.fixed_mask_count
+            return tf.multiply(inputs, tf.cast(current_mask, tf.float32))
+
         else:
-            return inputs
+            if self.mask_at_inference or training:
+                mask = tf.cast(tf.math.greater(tf.random.uniform([batch_size, feature_dim]), self.mask_rate), dtype=tf.float32)
+                mask = tf.expand_dims(mask, axis=1)
+                mask = tf.tile(mask, [1, sequence_len, 1])
+                return tf.multiply(inputs, mask)
+            else:
+                return inputs
 
 