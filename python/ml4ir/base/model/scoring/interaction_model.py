355fbea854905527e9cac97e0971285a7ea286e5
Ivan Koval <ivan.koval@icloud.com>
2025-01-14 16:08:37 +0100

    adding categorical_indices_op to the attributes of the interaction model

--- python/ml4ir/base/model/scoring/interaction_model.py
+++ python/ml4ir/base/model/scoring/interaction_model.py
@@ -114,7 +114,10 @@ class UnivariateInteractionModel(InteractionModel):
             feature_transform_op = self.__define_feature_transform_op(feature_info, file_io, **kwargs)
             if feature_transform_op:
                 self.feature_transform_ops[feature_node_name] = feature_transform_op
-            
+
+                if hasattr(feature_transform_op, 'categorical_indices_op'):
+                    self.categorical_indices_op = feature_transform_op.categorical_indices_op
+
         # Define a one-to-one feature transform for the label
         self.label_transform_op = self.__define_feature_transform_op(self.feature_config.get_label(), file_io, **kwargs)
 
